609e38424c16c49804c4e0b9cc68fa67484f219f
none <none@none.none>
2021-06-16 23:30:11 +0530

    123

--- app/src/main/java/nik/iant/physicscafe/Options.kt
+++ app/src/main/java/nik/iant/physicscafe/Options.kt
@@ -7,5 +7,6 @@ class Options : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_options)
+
     }
 }
